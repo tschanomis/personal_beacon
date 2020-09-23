4a70b53790d9b03dfb9a9ee4f6b044ddc54af5af
tschanomis <tschan.omis@gmail.com>
2020-09-23 18:46:28 +0200

    hooks and utils api on cardform

---
+++ virtualBeacon_front/src/Utils/API.js
@@ -0,0 +1,15 @@
+import axios from "axios";
+
+const LOCALURL = "http://localhost:8000/api";
+
+export default function RequestAPI(method = "GET", url = "/", data = []) {
+  return axios({
+    method: method,
+    url: LOCALURL + url,
+    data: data,
+    responseType: "json",
+    headers: {
+      "Content-Type": "application/json"
+    }
+  });
+}
--- virtualBeacon_front/src/components/CardForm.js
+++ virtualBeacon_front/src/components/CardForm.js
@@ -1,120 +1,111 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
 	Link,
 	Redirect,
 } from 'react-router-dom';
 
-import axios from 'axios';
+import RequestAPI from "../Utils/API";
 
 import './style/CardForm.css';
 
-class CardForm extends React.Component {
-	state = {
+export default function CardForm(props) {
+
+	const [manageLogin, setManageLogin] = useState({
 		isLoggedIn: false,
 		isError: false,
 		email: null,
 		password: null,
-		test: 14,
 		forgot: false,
-	}
+	})
 
-	handleChange = (e) => {
+	const handleChange = (e) => {
 		const value = e.target.value
-		this.setState({ ...this.state, [e.target.name]: value })
+		setManageLogin({ ...manageLogin, [e.target.name]: value })
 	}
 
-	handleSubmit = (e) => {
-		axios.post("http://ec2-18-218-63-27.us-east-2.compute.amazonaws.com:443/api/login", {
-			email: this.state.email,
-			password: this.state.password
+	const handleSubmit = (e) => {
+		RequestAPI("POST", "/login", {
+			email: manageLogin.email,
+			password: manageLogin.password
 		}).then(result => {
 			if (result.status === 200) {
-				this.props.getToken(result.data.success.token)
-				this.setState({ isLoggedIn: true })
-				this.props.alert(result.data.message)
+				props.getToken(result.data.success.token)
+				setManageLogin({ ...manageLogin, isLoggedIn: true })
+				props.alert(result.data.message)
 			} else {
-				this.setState({ isError: true })
-				this.props.alert(result.data.error)
+				setManageLogin({ ...manageLogin, isError: true })
+				props.alert(result.data.error)
 			}
 		}).catch(e => {
-			this.setState({ isError: true })
-			this.props.alert("Erreur connexion")
+			setManageLogin({ ...manageLogin, isError: true })
+			props.alert("Erreur connexion")
 		});
 		e.preventDefault()
 	}
 
-	handleForgot = (e) => {
-		axios.post("http://ec2-18-218-63-27.us-east-2.compute.amazonaws.com:443/api/password/email", {
-			email: this.state.email,
+	const handleForgot = (e) => {
+		RequestAPI("POST", "/password/email", {
+			email: manageLogin.email,
 		}).then(result => {
 			if (result.status === 200) {
-				this.props.alert("Email envoyé")
-				this.setState({ forgot: false })
+				props.alert("Email envoyé")
+				setManageLogin({ ...manageLogin, forgot: false })
 			} else {
-				this.props.alert(result.headers)
+				props.alert(result.headers)
 			}
 		}).catch(e => {
-			this.props.alert("erreur")
+			props.alert("Erreur")
 		});
 		e.preventDefault()
 	}
 
-	render() {
-		if (this.state.isLoggedIn) {
-			return <Redirect to="/dashboard" />;
-		}
+	if (manageLogin.isLoggedIn) {
+		return <Redirect to="/dashboard" />;
+	}
 
-		return (
-			<div className="CardForm" >
-				{this.state.forgot ?
-					<div className="CardForm-Container" style={{ justifyContent: 'center' }}>
-						<h3>Réinitialisation mot de passe:</h3>
-						<form className="CardForm-Login" onSubmit={this.handleForgot}>
-							<label for="email">adresse email</label>
-							<input type="email" name="email" id="email" placeholder="adresse email" onChange={this.handleChange} />
-							<input type="submit" value="REINITIALISER" className="Cardform-submit" style={{ border: 'none' }} />
-						</form>
-						<Link onClick={() => this.setState({ forgot: false })}>Retour</Link>
-					</div>
-					:
-					<div className="CardForm-Container">
-						<h1>Login</h1>
-						<p>Pas inscrit? <Link>Inscrivez vous</Link></p>
-						<form className="CardForm-Login" onSubmit={this.handleSubmit}>
-							<label for="email">adresse email</label>
-							<input type="email" name="email" id="email" placeholder="adresse email" onChange={this.handleChange} />
-							<label for="password">mot de passe</label>
-							<input type="password" name="password" id="password" placeholder="mot de passe" onChange={this.handleChange} />
-							<div className="CardForm-Login-options">
-								<div className="CardForm-Login-options-container-remember">
-									<input
-										name="remember"
-										id="remember"
-										type="checkbox"
-									/>
-									<label className="remember-label" for="remember">Se souvenir de moi</label>
-								</div>
-								<Link onClick={() => { this.setState({ forgot: true }) }}>Mot de passe oublié ?</Link>
+	return (
+		<div className="CardForm" >
+			{manageLogin.forgot ?
+				<div className="CardForm-Container" style={{ justifyContent: 'center' }}>
+					<h3>Réinitialisation mot de passe:</h3>
+					<form className="CardForm-Login" onSubmit={handleForgot}>
+						<label htmlFor="email">adresse email</label>
+						<input type="email" name="email" id="email" placeholder="adresse email" onChange={handleChange} />
+						<input type="submit" value="REINITIALISER" className="Cardform-submit" style={{ border: 'none' }} />
+					</form>
+					<Link to="" onClick={() => setManageLogin({ ...manageLogin, forgot: false })}>Retour</Link>
+				</div>
+				:
+				<div className="CardForm-Container">
+					<h1>Login</h1>
+					<p>Pas inscrit? <Link to="">Inscrivez vous</Link></p>
+					<form className="CardForm-Login" onSubmit={handleSubmit}>
+						<label htmlFor="email">adresse email</label>
+						<input type="email" name="email" id="email" placeholder="adresse email" onChange={handleChange} />
+						<label htmlFor="password">mot de passe</label>
+						<input type="password" name="password" id="password" placeholder="mot de passe" onChange={handleChange} />
+						<div className="CardForm-Login-options">
+							<div className="CardForm-Login-options-container-remember">
+								<input name="remember" id="remember" type="checkbox" />
+								<label className="remember-label" htmlFor="remember">Se souvenir de moi</label>
 							</div>
-							<input type="submit" value="CONNEXION" className="Cardform-submit" />
-						</form>
-						<div>
-							<a target="_blank" href="https://n-vibe.com/" rel="noopener noreferrer">n-vibe.com</a>
+							<Link to="" onClick={() => { setManageLogin({ ...manageLogin, forgot: true }) }}>Mot de passe oublié ?</Link>
 						</div>
-					</div>}
-
-				<div className="CardForm-Visu">
-					<div className="CardForm-Visu-Container">
-						<div className="CardForm-Visu-Logo"></div>
-						<div className="CardForm-Visu-Text">
-							<h1>Lorem ipsum</h1>
-							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean at dictum nisi. Nullam blandit ultrices ex, non pharetra urna dictum eu.</p>
-						</div>
+						<input type="submit" value="CONNEXION" className="Cardform-submit" />
+					</form>
+					<div>
+						<a target="_blank" href="https://google.com/" rel="noopener noreferrer">google.com</a>
+					</div>
+				</div>}
+			<div className="CardForm-Visu">
+				<div className="CardForm-Visu-Container">
+					<div className="CardForm-Visu-Logo"></div>
+					<div className="CardForm-Visu-Text">
+						<h1>Lorem ipsum</h1>
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean at dictum nisi. Nullam blandit ultrices ex, non pharetra urna dictum eu.</p>
 					</div>
 				</div>
 			</div>
-		);
-	}
+		</div>
+	);
 }
-
-export default CardForm;
