840f323532d569997a9590aa913392503ffd0462
tschanomis <tschan.omis@gmail.com>
2020-10-05 11:22:49 +0200

    leaflet min hooks

--- virtualBeacon_front/src/components/LeafletMin.js
+++ virtualBeacon_front/src/components/LeafletMin.js
@@ -1,37 +1,32 @@
 
-import React from "react";
+import React, { useState } from "react";
 import { Map, TileLayer } from "react-leaflet";
 
 import './style/LeafletMin.css';
 
-class Leaflet extends React.Component {
+export default function Leaflet(props) {
 
-	state = {
+	const [manageLeafletMin, setManageLeafletMin] = useState({
 		center: [48.854730, 2.346803],
-		zoom: 14,
-	}
+		zoom: 14
+	})
 
-	render() {
-		return (
-			<div className="LeafletMin">
-				<div className="Leaflet-min-block"></div>
-				<Map center={this.state.center} zoom={this.state.zoom} zoomControl={false}>
-					{this.props.satelite ?
-						<TileLayer
-							url="https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}"
-						//attribution='Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
-						/>
-						:
-						<TileLayer
-							url="http://{s}.tile.openstreetmap.se/hydda/full/{z}/{x}/{y}.png"
-						//attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-						/>
-					}
-				</Map >
-			</div>
-		);
-	}
+	return (
+		<div className="LeafletMin">
+			<div className="Leaflet-min-block"></div>
+			<Map center={manageLeafletMin.center} zoom={manageLeafletMin.zoom} zoomControl={false}>
+				{props.satelite ?
+					<TileLayer
+						url="https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}"
+					//attribution='Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
+					/>
+					:
+					<TileLayer
+						url="https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png"
+					//attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+					/>
+				}
+			</Map >
+		</div>
+	);
 }
-
-export default Leaflet
-
