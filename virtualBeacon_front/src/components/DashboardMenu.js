ffc935478dcc3f9baad5e06b92fd8eb2f383593f
tschanomis <tschan.omis@gmail.com>
2020-10-13 18:29:13 +0200

    fix bar menu

--- virtualBeacon_front/src/components/DashboardMenu.js
+++ virtualBeacon_front/src/components/DashboardMenu.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
+import { Link, NavLink } from 'react-router-dom';
 
 import './style/DashboardMenu.css';
-import { Link, NavLink } from 'react-router-dom';
 
 export default function DashboardMenu(props) {
 
@@ -18,29 +18,30 @@ export default function DashboardMenu(props) {
 	return (
 		<div className="DashboardMenu">
 			<div className="DashboardMenu-body">
-				<div className="DashboardMenu-body-item" id="1" onClick={handleClick} >
-					<NavLink to="/dashboard"><p style={{ borderBottom: manageDashboardMenu.selected === 1 ? '2px white solid' : 'none' }}>Dashboard</p></NavLink>
-				</div>
-				<div className="DashboardMenu-body-item" id="2" onClick={handleClick} >
-					<p style={{ borderBottom: manageDashboardMenu.selected === 2 ? '2px white solid' : 'none' }}>Import</p>
-				</div>
-				<div className="DashboardMenu-body-item" id="3" onClick={handleClick} >
-					<NavLink to="/dashboard/stats"><p style={{ borderBottom: manageDashboardMenu.selected === 3 ? '2px white solid' : 'none' }}>Statistiques</p></NavLink>
-				</div>
-			</div>
+				<NavLink to="/dashboard" className="inactive" style={{ borderBottom: manageDashboardMenu.selected === 1 ? '2px white solid' : 'none' }} id="1" onClick={handleClick}>
+					Dashboard
+				</NavLink>
+				<NavLink to="/dashboard" className="inactive" style={{ borderBottom: manageDashboardMenu.selected === 2 ? '2px white solid' : 'none' }} id="2" onClick={handleClick} >
+					Import
+				</NavLink>
+				<NavLink to="/dashboard/stats" className="inactive" style={{ borderBottom: manageDashboardMenu.selected === 3 ? '2px white solid' : 'none' }} id="3" onClick={handleClick}>
+					Statistiques
+				</NavLink>
+			</div >
 			<div className="DashboardMenu-account">
 				<p>Username / email</p>
 				<div className="DashboardMenu-account-picture"></div>
 				<span className="DashboardMenu-account-arrow" style={{ transform: manageDashboardMenu.displayAccount ? 'rotate(180deg)' : '' }} onClick={() => setManageDashboardMenu({ ...manageDashboardMenu, displayAccount: !manageDashboardMenu.displayAccount })}>v</span>
 			</div>
-			{manageDashboardMenu.displayAccount ?
-				< div className="DashboardMenu-account-options" style={{ animation: 'linear 0.1s slidein forwards' }}>
-					<Link to="Dashboard/">Compte</Link>
-					<Link to="Dashboard/" onClick={() => props.getTokenError()}>Déconnexion</Link>
-				</div>
-				:
-				null
+			{
+				manageDashboardMenu.displayAccount ?
+					< div className="DashboardMenu-account-options" style={{ animation: 'linear 0.1s slidein forwards' }}>
+						<Link to="Dashboard/">Compte</Link>
+						<Link to="Dashboard/" onClick={() => props.getTokenError()}>Déconnexion</Link>
+					</div>
+					:
+					null
 			}
-		</div>
+		</div >
 	)
 }
